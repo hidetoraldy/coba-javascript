bca65a9800834e34116d3acfb33d61239e864927
hidetoraldy <hidetoraldy@gmail.com>
2023-02-14 15:18:54 +0700

    belajar if else

---
+++ if else/angkot3.js
@@ -0,0 +1,12 @@
+var i = 1;
+    x = 10;
+    j = 6;
+
+for(i; i <= x; i++){
+    if(i <= 6){
+        console.log('Angkot No.' + i + ' sedang beroperasi.');
+    }else{
+        console.log('Angkot No.' + i + ' sedang rusak.');
+    }
+    
+}

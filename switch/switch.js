1b63c5e3b8e97c125205912f0aae4dfafe90fa90
hidetoraldy <hidetoraldy@gmail.com>
2023-02-15 09:52:57 +0700

    belajar switch

---
+++ switch/switch.js
@@ -0,0 +1,23 @@
+var i = prompt('masukan angka 1-5:');
+
+switch (i){
+    case '1':
+        alert('angka yang dimasukkan adalah '+ i);
+        break;
+    case '2':
+        alert('angka yang dimasukkan adalah '+ i);
+        break;
+    case '3':
+        alert('angka yang dimasukkan adalah '+ i);
+        break;
+    case '4':
+        alert('angka yang dimasukkan adalah '+ i);
+        break;
+    case '5':
+        alert('angka yang dimasukkan adalah '+ i);
+        break;
+    default:
+        alert('angka yang dimasukkan bukan 1-5!');
+        break;
+        
+}

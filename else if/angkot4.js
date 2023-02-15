e45b1f049cbeae2605c7f713d66debcdf594589e
hidetoraldy <hidetoraldy@gmail.com>
2023-02-15 09:28:30 +0700

    merubah angkot4.js

--- else if/angkot4.js
+++ else if/angkot4.js
@@ -3,9 +3,9 @@ var i = 1;
     j = 6;
 
 for(i; i <= x; i++){
-    if(i <= 6){
+    if(i <= 4 || i === 6){
         console.log('Angkot No.' + i + ' sedang beroperasi.');
-    }else if(i === 8){
+    }else if(i === 8 || i === 10 || i === 5){
         console.log('Angkot No.' + i + ' sedang lembur.');
     }else{
         console.log('Angkot No.' + i + ' sedang rusak.');

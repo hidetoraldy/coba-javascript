a55305503b58664659ab43243e48750cec7b30bc
hidetoraldy <hidetoraldy@gmail.com>
2023-02-17 08:48:47 +0700

    membuat function kubus

--- function part 1/kubus.js
+++ function part 1/kubus.js
@@ -1,4 +1,4 @@
-var kubus1 = prompt('masukkan sisi kubus 1:');
+/*var kubus1 = prompt('masukkan sisi kubus 1:');
     kubus2 = prompt('masukkan sisi kubus 2:');
 
 var volume1 = kubus1**3;
@@ -6,4 +6,16 @@ var volume1 = kubus1**3;
 
 var total = volume1 + volume2;
 
-alert('total kedua volume kubus adalah: ' + total);
+alert('total kedua volume kubus adalah: ' + total);*/
+
+function jumlahVolumeDuaKubus(sisi1, sisi2) {
+    var total;
+
+    var volumeKubus1 = sisi1 ** 3;
+        volumeKubus2 = sisi2 ** 3;
+
+    var total = volumeKubus1 + volumeKubus2;
+    return total;
+}
+
+alert(jumlahVolumeDuaKubus(8, 3));

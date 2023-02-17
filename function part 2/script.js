9aba9a9fdbbcdc505a54fdb27ec31fc616a0237d
hidetoraldy <hidetoraldy@gmail.com>
2023-02-17 09:18:48 +0700

    belajar arguments array

---
+++ function part 2/script.js
@@ -0,0 +1,11 @@
+function penjumlahan (){
+    var hasil = 0;
+
+    for(var i = 0; i < arguments.length; i++){
+        hasil += arguments[i];
+    }
+    return hasil;
+}
+
+var coba = penjumlahan(1,2,3,4);
+console.log(coba);

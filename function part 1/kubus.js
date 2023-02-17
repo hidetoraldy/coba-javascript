35282dae1ebd86bf419ebf92a4c75d8b54cd0052
hidetoraldy <hidetoraldy@gmail.com>
2023-02-17 08:36:15 +0700

    belajar function

---
+++ function part 1/kubus.js
@@ -0,0 +1,9 @@
+var kubus1 = prompt('masukkan sisi kubus 1:');
+    kubus2 = prompt('masukkan sisi kubus 2:');
+
+var volume1 = kubus1**3;
+    volume2 = kubus2**3;
+
+var total = volume1 + volume2;
+
+alert('total kedua volume kubus adalah: ' + total);
